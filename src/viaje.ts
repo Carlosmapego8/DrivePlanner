7d37eead248c3c081eb24e084c262ddc1ee170ba
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-02 20:24:03 +0100

    closes FerniCuesta#11
    Se han añadido nuevos datos que se almacenaran en el objeto valor viaje, obteniendo así una mejor representación. En un principio un viaje no tiene que tener asignado ningún conductor o pasajeros, pues estos los asignará la entidad encargada de gestionar los viajes más adelante

--- src/viaje.ts
+++ src/viaje.ts
@@ -4,9 +4,13 @@ import { Profesor } from "./profesor";
 export class Viaje{
     private _fecha: Date;
     private _kilometros: number;
+    private _conductor: Profesor;
+    private _pasajeros: Profesor[];
 
     constructor(fecha: Date, kilometros: number){
         this._fecha = fecha;
-        this._kilometros = kilometros;      
+        this._kilometros = kilometros; 
+        this._conductor = null;
+        this._pasajeros = []
     }
 }
