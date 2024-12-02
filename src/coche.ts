baa7786a91fe3778090b98b4e9180d6b1edc09f5
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-02 17:17:13 +0100

    closes FerniCuesta#13
    Se decide modelar un coche como entidad separada ya que dependiendo del coche que un profesor posea puede tener un mayor o menor número de plazas, permitiendo así una mayor flexibilidad. Se añade la matricula como id único que identificará a cada coche del resto.

---
+++ src/coche.ts
@@ -0,0 +1,9 @@
+export class Coche{
+    private _matricula: string;
+    private _capacidad: number;
+
+    constructor(matricula: string, capacidad: number){
+        this._matricula = matricula;
+        this._capacidad = capacidad;
+    }
+}
