8151f8a71f26b4da080f771f2fa653152f1b79d4
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-04 11:11:03 +0100

    closes FerniCuesta#12
    Se añaden los campos horaEntrada y horaSalida dentro del objeto valor profesor para contemplar los distintos horarios de los profesores que influenciarán en el reparto de viajes. Se decide hacerlo en un formato de string por simplicidad a la hora de su manejo y que estos solo se utilizarán para comparaciones básicas en un futuro, no se necesitará aplicar lógica compleja sobre estas horas.

--- src/profesor.ts
+++ src/profesor.ts
@@ -3,9 +3,13 @@ import { Coche } from "./coche";
 export class Profesor{
     private _nombre: string;
     private _coches: Coche[];
+    private _horaEntrada: string; //En formato ""HH:mm"
+    private _horaSalida: string; //En formato ""HH:mm"
 
-    constructor(nombre: string, coches: Coche[]){
+    constructor(nombre: string, coches: Coche[], horaEntrada: string, horaSalida: string){
         this._nombre = nombre;
         this._coches = coches;
+        this._horaEntrada = horaEntrada;
+        this._horaSalida = horaSalida;
     }
 }
