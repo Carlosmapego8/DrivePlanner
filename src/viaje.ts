1eb3c48080a1fc1bc8a8a6bcbd71f822435c1453
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-04 11:25:13 +0100

    closes FerniCuesta#11
    Se incluye el campo plazasOcupadas. Este campo permite el tener en cuenta situaciones en las que la capacidad inicial del coche se vea afectada por acompañantes externos o cualquier otra situación. De esta forma la capacidad del coche se verá disminuida en plazasOcupadas unidades

--- src/viaje.ts
+++ src/viaje.ts
@@ -8,13 +8,15 @@ export class Viaje{
     private _pasajeros: Profesor[];
     private _salida: string;
     private _coche: Coche;
+    private _plazasOcupadas: number;
 
-    constructor(fecha: Date, kilometros: number, salida: string){
+    constructor(fecha: Date, kilometros: number, salida: string, plazasOcupadas: number){
         this._fecha = fecha;
         this._kilometros = kilometros; 
         this._conductor = null;
         this._coche = null;
         this._pasajeros = [];
         this._salida = salida;
+        this._plazasOcupadas = number;
     }
 }
