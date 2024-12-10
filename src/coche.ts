473a5afbb328d31c5a7f6e9411d6a09c90ba5572
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-10 12:20:20 +0100

    Descripción coche

--- src/coche.ts
+++ src/coche.ts
@@ -3,6 +3,12 @@ export class Coche{
     private _capacidad: number;
 
     constructor(matricula: string, capacidad: number){
+          /**
+         * Representa un coche
+         * _matricula: representa la matricula del coche que lo identifica del resto de coches
+         * _capacidad: representa la capacidad de pasajeros máxima del coche
+         */
+    
         if (capacidad < 0) {
             throw new Error("La capacidad de un coche tiene que ser un numero positivo.");
         }
