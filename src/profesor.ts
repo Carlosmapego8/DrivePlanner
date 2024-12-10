5d799175223b20a3272a3d73a115b1292bb237dc
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-10 18:38:15 +0100

    Cambio representacion coches, #13
    Se modifica la representacion de los coches del profesor

--- src/profesor.ts
+++ src/profesor.ts
@@ -1,20 +1,18 @@
-import { Coche } from "./coche";
-
 export class Profesor{
     /**
     * Representa a un profesor
     * _nombre: nombre del profesor que lo identifica del resto de profesores
-    * _coches: representan los identificadores de los coches que el profesor posee.
+    * _coches: representan las matriculas y la capacidad de los coches del profesor.
     * _horaEntrada: hora a la que el profesor entra a trabajar en formato "HH:mm"
     * _horaSalida: hora a la que el profesor sale de trabajar en formato "HH::mm"
     */
     
     private _nombre: string;
-    private _coches: string[];
+    private _coches: Record<string, int>;
     private _horaEntrada: string; //En formato ""HH:mm"
     private _horaSalida: string; //En formato ""HH:mm"
 
-    constructor(nombre: string, coches: string[], horaEntrada: string, horaSalida: string){
+    constructor(nombre: string, coches: Record<string, int>, horaEntrada: string, horaSalida: string){
         this._nombre = nombre;
         this._coches = coches;
         this._horaEntrada = horaEntrada;
