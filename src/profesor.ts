93b6a0c423ad4d9fd98ecd2d6aee2d17e3700105
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-10 11:48:49 +0100

    fix FerniCuesta#12
    Se cambia la forma de almacenar los coches de un profesor, consiguiendo de esta forma SSOT

--- src/profesor.ts
+++ src/profesor.ts
@@ -2,11 +2,11 @@ import { Coche } from "./coche";
 
 export class Profesor{
     private _nombre: string;
-    private _coches: Coche[];
+    private _coches: string[];
     private _horaEntrada: string; //En formato ""HH:mm"
     private _horaSalida: string; //En formato ""HH:mm"
 
-    constructor(nombre: string, coches: Coche[], horaEntrada: string, horaSalida: string){
+    constructor(nombre: string, coches: string[], horaEntrada: string, horaSalida: string){
         this._nombre = nombre;
         this._coches = coches;
         this._horaEntrada = horaEntrada;
