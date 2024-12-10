715e46dc61f390da9953b61571d9844aa625648c
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-10 12:05:49 +0100

    fix FerniCuesta#13
    Se añade la comprobación explícita del valor del atributo capacidad

--- src/coche.ts
+++ src/coche.ts
@@ -3,6 +3,9 @@ export class Coche{
     private _capacidad: number;
 
     constructor(matricula: string, capacidad: number){
+        if (capacidad < 0) {
+            throw new Error("La capacidad de un coche tiene que ser un numero positivo.");
+        }
         this._matricula = matricula;
         this._capacidad = capacidad;
     }
