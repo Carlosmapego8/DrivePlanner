764b0653661a43433feb2a88f7482088913186a4
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-10 12:22:29 +0100

    Documentación gestorviajes

--- src/gestorviajes.ts
+++ src/gestorviajes.ts
@@ -4,7 +4,11 @@ import { Viaje } from "./viaje";
 
 
 class GestorViajes{
-
+     /**
+         * Entidad encargada de realizar la lógica de negocio, asignando los viajes a los distintos profesores
+         * _viajesDisponibles: representan los identificadores de los viajes que aún no están asignados
+         * _profesoresDisponibles: representan los identificadores de los profesores que necesitan tener un viaje asignado
+         */
     private _viajesDisponibles: Date[];
     private _profesoresDisponibles: string[];
 
