5af41375c905ad9ac3025407f8e9977bbcb22b58
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-10 12:25:27 +0100

    Descripción profesor

--- src/profesor.ts
+++ src/profesor.ts
@@ -1,6 +1,14 @@
 import { Coche } from "./coche";
 
 export class Profesor{
+    /**
+    * Representa a un profesor
+    * _nombre: nombre del profesor que lo identifica del resto de profesores
+    * _coches: representan los identificadores de los coches que el profesor posee.
+    * _horaEntrada: hora a la que el profesor entra a trabajar en formato "HH:mm"
+    * _horaSalida: hora a la que el profesor sale de trabajar en formato "HH::mm"
+    */
+    
     private _nombre: string;
     private _coches: string[];
     private _horaEntrada: string; //En formato ""HH:mm"
