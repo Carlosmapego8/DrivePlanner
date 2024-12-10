224f859dd0a5829e4cf5a5437931a7d04ed09741
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-10 11:57:01 +0100

    fix FerniCuesta#11
    Se eliminan los kilómetros, pues no será necesaria que un usuario los introduzca

--- src/viaje.ts
+++ src/viaje.ts
@@ -3,16 +3,14 @@ import { Profesor } from "./profesor";
 
 export class Viaje{
     private _fecha: Date;
-    private _kilometros: number;
     private _conductor: string;
     private _pasajeros: string[];
     private _salida: string;
     private _coche: string;
     private _plazasOcupadas: number;
 
-    constructor(fecha: Date, kilometros: number, salida: string, plazasOcupadas: number){
+    constructor(fecha: Date, salida: string, plazasOcupadas: number){
         this._fecha = fecha;
-        this._kilometros = kilometros; 
         this._conductor = null;
         this._coche = null;
         this._pasajeros = [];
