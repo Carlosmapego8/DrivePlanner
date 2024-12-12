791bfb8998d3d68f0133333050a67f74d4f01861
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-12 10:01:29 +0100

    Cambio representación viajes y profesores, #15
    Se cambia la representación interna de los viajes y profesores disponibles a un tipo set para mantener el SSOT

--- src/gestorviajes.ts
+++ src/gestorviajes.ts
@@ -8,10 +8,10 @@ class GestorViajes{
          * _viajesDisponibles: representan los identificadores de los viajes que aún no están asignados
          * _profesoresDisponibles: representan los identificadores de los profesores que necesitan tener un viaje asignado
          */
-    private _viajesDisponibles: Date[];
-    private _profesoresDisponibles: string[];
+    private _viajesDisponibles: Set<Date>;
+    private _profesoresDisponibles: Set<string>;
 
-    constructor(viajes: Date[], profesores: string[]){
+    constructor(viajes: Set<Date>, profesores: Set<string>){
         this._viajesDisponibles = viajes;
         this._profesoresDisponibles = profesores;
     }
