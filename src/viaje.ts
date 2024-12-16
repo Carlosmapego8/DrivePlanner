51b321a4f3fe06965d984eb003d97e0f0f873337
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-16 13:31:09 +0100

    Cambio representación viajes, #15
    Se cambia la representacion para que los viajes sean un tipo nuevo de datos, que sera gestionado por la clase AsignacionViajes. Esta clase sera la encargada de asegurar que a cada profesor le corresponde un viaje en una fecha concreta

--- src/viaje.ts
+++ src/viaje.ts
@@ -1,31 +1,23 @@
 import { Profesor } from "./profesor";
 
-export class Viaje{
-     /**
+export type Viaje{
+      /**
      * Representa un viaje
      * _fecha: representa la fecha en la que se realizará el viaje. Identifica al viaje por completo
-     * _conductor: representa el identificador al conductor del viaje, se calculará por la lógica de negocio. 
-     * _pasajeros: representan a los identificadores a los pasajeros del viaje, los calculará la lógica de negocio.
      * _salida: representa la hora del día a la que saldrá el viaje
      * _plazasOcupadas: representan las plazas ocupadas en un viaje (por acompañantes, transporte de objetos, ...), este valor reducirá la capacidad inicial del coche
      */
+    readonly _fecha: Date;
+    readonly _horaSalida: string;
+    readonly _plazasOcupadas: number;
     
-    private readonly _fecha: Date;
-    private _conductor: Profesor;
-    private _pasajeros: Set<Profesor>;
-    private _horaSalida: string;
-    private readonly _plazasOcupadas: number;
+}
 
-    constructor(fecha: Date, plazasOcupadas: number){
-        
-        if (plazasOcupadas < 0) {
-            throw new Error("Las plazas ocupadas tienen que ser un numero positivo.");
-        }
-        
-        this._fecha = fecha;
-        this._conductor = null;
-        this._horaSalida = ""
-        this._pasajeros = new Set();
-        this._plazasOcupadas = number;
+export class AsignacionViajes{
+   
+    private _asignaciones: Record<Viaje, Set<Profesor>>;   
+
+    constructor(){
+         this._asignaciones = New Record();
     }
 }
