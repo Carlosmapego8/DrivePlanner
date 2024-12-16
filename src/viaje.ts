beae120742242932cca64efd9880b828fdc6ae11
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2024-12-16 17:07:06 +0100

    Eliminación plazas_ocupadas, #11
    Al no estar reflejado en la HU no es necesario modelarlo

--- src/viaje.ts
+++ src/viaje.ts
@@ -5,11 +5,9 @@ export type Viaje{
      * Representa un viaje
      * _fecha: representa la fecha en la que se realizará el viaje. Identifica al viaje por completo
      * _salida: representa la hora del día a la que saldrá el viaje
-     * _plazasOcupadas: representan las plazas ocupadas en un viaje (por acompañantes, transporte de objetos, ...), este valor reducirá la capacidad inicial del coche
      */
     readonly _fecha: Date;
     readonly _horaSalida: string;
-    readonly _plazasOcupadas: number;
     
 }
 
